cc02ab0a06455cd7d64202348067ec82e9275ad4
Kiitsak Pharaphan <kungpotato2008@hotmail.com>
2019-05-07 17:39:11 +0700

    add-color-picker

--- src/components/DialogEdit.jsx
+++ src/components/DialogEdit.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Dialog from '@material-ui/core/Dialog'
 import Paper from '@material-ui/core/Paper'
 import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
 import TextField from '@material-ui/core/TextField'
+import { observer } from 'mobx-react'
+import { observable, action } from 'mobx'
+import { ChromePicker } from 'react-color'
 
 const style = {
   dialogStyle: {
@@ -19,60 +22,91 @@ const style = {
     color: '#fff'
   }
 }
-const DialogEdit = ({
-  open,
-  saveItem,
-  removeItem,
-  closeDialog,
-  handleChange,
-  title,
-  colorBackGround,
-  handleChangeColor
-}) => (
-  <Dialog open={open}>
-    <Paper style={style.dialogStyle}>
-      <Grid container style={{ padding: '24px' }}>
-        <Grid item xs={6}>
-          <TextField
-            fullWidth
-            label="Title"
-            value={title}
-            onChange={handleChange}
+
+@observer
+class DialogEdit extends Component {
+  @observable
+  dialogOpen = false
+
+  @action
+  handleChangeComplete = ({ hex }) => {
+    const { handleChangeColor } = this.props
+    handleChangeColor({ target: { value: hex } })
+  }
+
+  @action.bound
+  toggleDialog() {
+    this.dialogOpen = !this.dialogOpen
+  }
+
+  render() {
+    const {
+      open,
+      saveItem,
+      removeItem,
+      closeDialog,
+      handleChange,
+      title,
+      colorBackGround,
+      handleChangeColor
+    } = this.props
+    return (
+      <React.Fragment>
+        <Dialog open={this.dialogOpen}>
+          <Button onClick={this.toggleDialog}>close</Button>
+          <ChromePicker
+            color={this.colorBG}
+            onChangeComplete={this.handleChangeComplete}
           />
-          <TextField
-            fullWidth
-            label="color code"
-            value={colorBackGround}
-            onChange={handleChangeColor}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <Button
-            variant="contained"
-            color="primary"
-            style={style.ButtonStyle}
-            onClick={saveItem}
-          >
-            Save
-          </Button>
-          <Button
-            variant="contained"
-            style={style.removeButtonStyle}
-            onClick={removeItem}
-          >
-            Remove
-          </Button>
-          <Button
-            variant="contained"
-            style={style.ButtonStyle}
-            onClick={closeDialog}
-          >
-            Close
-          </Button>
-        </Grid>
-      </Grid>
-    </Paper>
-  </Dialog>
-)
+        </Dialog>
+        <Dialog open={open}>
+          <Paper style={style.dialogStyle}>
+            <Grid container style={{ padding: '24px' }}>
+              <Grid item xs={6}>
+                <TextField
+                  fullWidth
+                  label="Title"
+                  value={title}
+                  onChange={handleChange}
+                />
+                <TextField
+                  onClick={this.toggleDialog}
+                  fullWidth
+                  label="color code"
+                  value={colorBackGround}
+                  onChange={handleChangeColor}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={style.ButtonStyle}
+                  onClick={saveItem}
+                >
+                  Save
+                </Button>
+                <Button
+                  variant="contained"
+                  style={style.removeButtonStyle}
+                  onClick={removeItem}
+                >
+                  Remove
+                </Button>
+                <Button
+                  variant="contained"
+                  style={style.ButtonStyle}
+                  onClick={closeDialog}
+                >
+                  Close
+                </Button>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Dialog>
+      </React.Fragment>
+    )
+  }
+}
 
 export default DialogEdit
